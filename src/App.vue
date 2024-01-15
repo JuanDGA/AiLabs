b48878c9e2a3823e8a9b99157eb39c3ba0788aca
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-01-15 09:31:51 -0500

    Landing page look improved

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,22 @@
 </script>
 
 <template>
-  <div>Profile Cards</div>
-  <button class="bg-blue-500 text-white font-bold rounded-lg px-3 py-1 cursor-pointer hover:bg-blue-600 disabled:cursor-progress disabled:bg-blue-400" @click="ask" :disabled="loading">Generate</button>
-  <ProfileCard :profile="response" />
+  <div class="bg-gray-50 min-h-screen">
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-semibold mb-2">Profile Cards Generator</h1>
+      <h3 class="font-medium text-gray-500 mb-4">To start, please press the generate button</h3>
+      <button
+        class="bg-blue-500 text-white font-bold rounded-lg px-4 py-2 cursor-pointer hover:bg-blue-600 disabled:cursor-not-allowed disabled:bg-blue-400"
+        @click="ask"
+        :disabled="loading"
+      >
+        {{ loading ? 'Generating...' : 'Generate' }}
+      </button>
+
+      <div class="mt-8">
+        <ProfileCard v-if="response" :profile="response" />
+      </div>
+    </div>
+  </div>
 </template>
+
